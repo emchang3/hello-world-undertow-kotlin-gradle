86f5326466bf20f430bf16e8b8bbf5d5e8850443
Ezra M. Chang <ezchang@expedia.com>
2020-08-08 21:17:15 -0500

    chore: cleans up router method in server class

--- src/main/kotlin/HelloWorldServer.kt
+++ src/main/kotlin/HelloWorldServer.kt
@@ -1,31 +1,10 @@
 import com.fasterxml.jackson.databind.ObjectMapper
-import io.undertow.Handlers
 import io.undertow.Undertow
-import io.undertow.UndertowOptions
-import io.undertow.server.RoutingHandler
-import io.undertow.util.Headers
-import routes.ROUTE_CONFIG
+
+import routes.router
 
 object HelloWorldServer {
     private val objectMapper = ObjectMapper()
-
-    private fun router(objectMapper: ObjectMapper): RoutingHandler {
-        val router = Handlers.routing()
-
-        ROUTE_CONFIG.entries.forEach { verb ->
-            verb.value.entries.forEach { route ->
-                router.add(verb.key, route.key, route.value(objectMapper))
-            }
-
-            router.setFallbackHandler { exchange ->
-                exchange.responseHeaders.put(Headers.CONTENT_TYPE, "text/plain;charset=utf-8")
-                exchange.statusCode = 404
-                exchange.responseSender.send("無")
-            }
-        }
-
-        return router
-    }
 
     @JvmStatic
     fun main(args: Array<String>) {
